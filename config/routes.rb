335f16cdd66597544f10bf081e810a2d54166408
Gareth Gomersall <ggomersall@gmail.com>
2016-10-15 16:55:05 +0100

    created some fake data, setup resources for messages and added textarea for the message field

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     # resource should be singular as it's just for individual user
     # this resource also only allows for joining and leaving channels
     resource :chatroom_users
+    resources :messages
   end
 
   root to: "chatrooms#index"
