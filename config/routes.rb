73a35ed887a8b3cdfdc6cc068ec0c71a9c0f4ade
Gareth Gomersall <ggomersall@gmail.com>
2016-10-15 16:21:43 +0100

    created controller for chatroom_users, added links to join and leave channels

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :chatrooms
+  resources :chatrooms do
+    # resource should be singular as it's just for individual user
+    resource :chatroom_users
+  end
 
   root to: "chatrooms#index"
 end
